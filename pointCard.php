10048dc950881776c7943e5f406523c0a876c0c1
jiayuKuo <102267757+jiayuKuo@users.noreply.github.com>
2022-09-20 13:21:10 -0600

    Update pointCard.php

--- pointCard.php
+++ pointCard.php
@@ -1,4 +1,19 @@
 <?php
+if(isset($_COOKIE['point'])){
+ echo" <p> you have".$_POST['point']. "points </p>";
+
+}elseif(!isset($_COOKIE['point'])){
+    $point=1;
+    setcookie('point',$point, strtotime("+1 year"));
+    echo" <p> you have".$_POST['point']. "point.</p>";
+}elseif($_COOKIE['point']==8){
+    echo "<p>Congratulations! You got 8 Points, This is your coffee coupon</p>";//小七店員：機八點 換咖啡喔<3 
+}
+   
+
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -12,8 +27,30 @@
 </head>
 <body>
     <header>
-        <h1>test</h1>
+        <h1>Point Card</h1>
     </header>
+
+<form action="pointCard.php" method="post">
+<legend>How many colour in the Canada flag?</legend>
+<label for="1">1</label>
+<input type="radio" name="1" id="1">
+<label for="1">2</label>
+<input type="radio" name="2" id="2">
+<label for="1">3</label>
+<input type="radio" name="3" id="3">
+<?php
+$_COOKIES['point'];
+?>
+
+<legend>How many colour in Janpan flag?</legend>
+<label for="1">1</label>
+<input type="radio" name="1" id="1">
+<label for="1">2</label>
+<input type="radio" name="2" id="2">
+<label for="1">3</label>
+<input type="radio" name="3" id="3">
+
+ 
 
 </body>
 </html>
