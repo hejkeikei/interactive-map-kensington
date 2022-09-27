967cf15d6867112aba168dd291390f3de8168823
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-09-27 14:14:49 -0600

    2022-09-27 update
    - index switch to map
    - mobile first style setup
    - breakpoint set
    - building info section layout
    - implement actual map

--- index.php
+++ index.php
@@ -1,37 +1,17 @@
 <?php
 include("header.php");
 ?>
-<section id="about">
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos iste
-    dolorem necessitatibus dolor aspernatur corporis error. A, voluptates
-    dolore. Ad assumenda dolorem dignissimos iste architecto. Neque
-    molestias doloremque numquam ratione. Quisquam, architecto veniam
-    dolores voluptates eaque vitae ducimus esse rerum! In cupiditate
-    quisquam debitis reiciendis amet enim perferendis saepe quibusdam,
-    laborum, perspiciatis ex. Illum ipsa optio blanditiis quaerat quia
-    velit pariatur suscipit quam ducimus architecto cumque autem quibusdam
-    tempore veniam repellendus, aliquid ex doloribus magni! Accusantium
-    exercitationem doloremque rem ipsam corporis cupiditate eum totam
-    ratione, facilis repellendus alias, ipsa dolore, expedita molestiae?
-    Minima, eos quia sequi deleniti ipsum amet vero.
-  </p>
-  <div class="row">
-    <div class="imgBox">
-      <img src="" alt="" width="" height="">
-    </div>
-    <div class="imgBox">
-      <img src="" alt="" width="" height="">
-    </div>
-    <div class="imgBox">
-      <img src="" alt="" width="" height="">
-    </div>
-  </div>
-</section>
+<div id="map">
+  <!-- tempoary SVG element ***just for testing*** -->
+  <?php include("svg_map.php"); ?>
+</div>
+
+
 </main>
 <?php
 include("footer.php");
 ?>
+<script src="map.js"></script>
 </body>
 
 </html>
