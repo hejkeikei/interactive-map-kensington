4d85e815f159fd41d6d110c7dbd74383ecad34b7
jiayuKuo <102267757+jiayuKuo@users.noreply.github.com>
2022-10-11 09:47:53 -0600

    building.php
    set cookie for remember user answer the question

--- building.php
+++ building.php
@@ -62,60 +62,72 @@ include("footer.php");
     <?php
   
     echo "var answer ='" . $row['answer'] . "';";
-    echo "var id ='".$id ."'";
+    echo "var id ='".$id ."';";
     ?>
-    //use loop to get radio value
-    //var userInput = 
-    // when (if)user click "send" button{
+      // set expireday for cookie
+      var now = new Date();
+      var exday = now.getTime() + (60*60*24); 
+          console.log(exday); 
+
+    if(getCookie(id)===""){
+        setCookie(id,"false",30);
+        
+    }else{
+        console.log(getCookie(id));
+    }
 
     send.addEventListener('click', function() {
-    
         var userInput = document.querySelector('input[name=option]:checked').value;
         console.log(userInput);
         console.log(answer);
-        let points = document.cookie;
-        console.log(points);
-        var actualPoints = parseInt(points.split('=')[1]);//conver
+        // let points = document.cookie;
+        var points = getCookie('point');
+        console.log("get cookie point:"+points);
+        // var actualPoints = parseInt(points.split('=')[1]);//conver
+        var actualPoints = points;
         // console.log("points splits: ", actualPoints);
-        if (userInput === answer) { 
+        if (userInput === answer){ 
             console.log("Correct Answer!!")
-            console.log("type: ", typeof points); // string
-
-            // set expireday for cookie
-            var now = new Date();
-            var exday = now.getTime() + (60*60*24); 
-            console.log(exday); 
+            // console.log("type: ", typeof points); // string  
+          
             
             //need set another cookie in here for remember user come before,if they didn't came before then actualPoints++; 
-            document.cookie= "correctAns= A"+id+";expires="+ exday +";";
-            // if(getCookie('correctAns')){}
-            // if(getCookie('correctAns')){
+            
+            // document.cookie= id+ "=true;expires="+ exday +";";
+  
+            if(getCookie(id)==='true'){
+                console.log("you have been answer this question and got one point before");
+            }else{
+                setCookie(id,'true',30);
+                if (points !== "") {
+                    actualPoints++;
+                    console.log("Add 1 point.");
+                    console.log("Your total point is " + actualPoints);
+                    // if point is less than 10, set a update cookie, 
+                        if(actualPoints<=9){
+                            setCookie("point",actualPoints,30);
+                            // document.cookie = "points=" + actualPoints + ";expires="+ exday +";";
+                        }         
+                } else {
+                    //if there no cookie, set cookie for point
+                    console.log("else ", actualPoints);
+                    actualPoints = 0;
+                    setCookie("point",1,30);
+                    //document.cookie = "points=1; expires="+ exday +";"; //not sure I am doing right
+                    // console.log(points);
+                }
+
+            }
                   
             
-                    if (points !== "") {
-                        actualPoints++;
-                        console.log("Add 1 point.");
-                        console.log("Your total point is " + actualPoints);
-                        // if point is less than 10, set a update cookie, 
-                            if(actualPoints<=9){
-                                document.cookie = "points=" + actualPoints + ";expires="+ exday +";";
-                            }
-                        
-                    } else {
-                        //if there no cookie, set cookie for point
-                        console.log("else ", actualPoints);
-                        actualPoints = 0
-                        document.cookie = "points=1; expires="+ exday +";"; //not sure I am doing right
-                        console.log(points);
-                    }
-            }
+                
+          
 
-        } else { 
-            console.log("Sorry! You got the worng answer, Please try again.")
+        }else{ 
+            console.log("Sorry! You got the worng answer, Please try again.");
         }
-
-    //}
-    )
+    });
+    
 
 function getCookie(cname) {
   let name = cname + "=";
