6d128c434c326f3b8d44e1eb6fc099feaa9b35d3
jiayuKuo <102267757+jiayuKuo@users.noreply.github.com>
2022-10-11 08:07:31 -0600

    building update
    set point limit is 10

--- building.php
+++ building.php
@@ -60,52 +60,92 @@ include("footer.php");
 <script src="building_info.js"></script>
 <script>
     <?php
-    // this variable is for compare anwser 
+  
     echo "var answer ='" . $row['answer'] . "';";
+    echo "var id ='".$id ."'";
     ?>
     //use loop to get radio value
     //var userInput = 
     // when (if)user click "send" button{
 
     send.addEventListener('click', function() {
-        // var userInput = get radio button value(A,B or C)
+    
         var userInput = document.querySelector('input[name=option]:checked').value;
         console.log(userInput);
         console.log(answer);
         let points = document.cookie;
         console.log(points);
-
         var actualPoints = parseInt(points.split('=')[1]);//conver
         // console.log("points splits: ", actualPoints);
-        if (userInput === answer) {
+        if (userInput === answer) { 
             console.log("Correct Answer!!")
-            console.log("type: ", typeof points); // string,should be number
-            //need set another cookie in here for remember user come before,then show you 
+            console.log("type: ", typeof points); // string
 
-            if (points !== "") {
-                actualPoints++;
-                console.log("Add 1 point.");
-                console.log("Your total point is " + actualPoints);
-                // if point is less than 10, set a update cookie, 
-                    if(actualPoints<=9){
-                        document.cookie = "points=" + actualPoints + "; expires= 1 year;";
+            // set expireday for cookie
+            var now = new Date();
+            var exday = now.getTime() + (60*60*24); 
+            console.log(exday); 
+            
+            //need set another cookie in here for remember user come before,if they didn't came before then actualPoints++; 
+            document.cookie= "correctAns= A"+id+";expires="+ exday +";";
+            // if(getCookie('correctAns')){}
+            // if(getCookie('correctAns')){
+                  
+            
+                    if (points !== "") {
+                        actualPoints++;
+                        console.log("Add 1 point.");
+                        console.log("Your total point is " + actualPoints);
+                        // if point is less than 10, set a update cookie, 
+                            if(actualPoints<=9){
+                                document.cookie = "points=" + actualPoints + ";expires="+ exday +";";
+                            }
+                        
+                    } else {
+                        //if there no cookie, set cookie for point
+                        console.log("else ", actualPoints);
+                        actualPoints = 0
+                        document.cookie = "points=1; expires="+ exday +";"; //not sure I am doing right
+                        console.log(points);
                     }
-                
-            } else {
-                //if there no cookie, set cookie for point
-                console.log("else ", actualPoints);
-                actualPoints = 0
-                document.cookie = "points=1; expires= 1 year;";
-                console.log(points);
             }
 
         } else { 
             console.log("Sorry! You got the worng answer, Please try again.")
         }
 
+    //}
+    )
 
-    });
-
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+    //     
+    //     compare var answer(A,B or C) and var userInput(A,B or C)
+    //     if(answer===userInput){
+    //            if(isset cookie){
+    //                    getcookie points
+    //                     point + 1
+    //                     display message "Point add 1"
+    //                  }else{
+    //                        set cookie
+    //                   }
+    //     }else{
+    //         display message "Wrong guess again"
+    //     }
+    // }
 </script>
 <script src="building_info.js"></script>
 </body>
